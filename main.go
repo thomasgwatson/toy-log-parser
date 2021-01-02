7edeb1205d997a583826d0e0437b28cb2b41cde0
Tom Watson <thomasgabriel.watson@gmail.com>
2021-01-02 14:22:32 -0800

    Test CI/CD

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("hello world")
+
+	// -in string
+	// -out string
+	// -max-client-ips int
+	// -max-paths int
+
+	args := os.Args[1:]
+	fmt.Println(args)
+
+	wordPtr := flag.String("in", "log.txt", "input file")
+	flag.Parse()
+	fmt.Println(*wordPtr)
 }
