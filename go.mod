fd27c43270e8ba65c9afc31b05ae6de576985115
Tom Watson <thomasgabriel.watson@gmail.com>
2021-01-10 12:33:42 -0800

    Implement ParseLine regex with tests

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module log-parser
 
 go 1.15
-
-require github.com/satyrius/gonx v1.3.0
--- utils.go
+++ utils.go
@@ -3,7 +3,8 @@ package main
 import "regexp"
 
 func ParseLine(line string) (match []string) {
-	var logRegex = regexp.MustCompile(`^(\S+)[\s-]+\[([^][]*)]\s+"GET\s+([^"]+)"[^"](\d{3})[^"](\d+)`)
+	var logRegex = regexp.MustCompile(`^(\S+)[\s-]-\s\S+\s\[[^][]*]\s"\S+\s(/[^\?\s]*)\?*\S*\s[^"]+"\s\d{3}\s(\d+)\s"([^"]+)"`)
+	//regex workspace https://regex101.com/r/I7EPUI/3
 	match = logRegex.FindStringSubmatch(line)
 	return
 }
