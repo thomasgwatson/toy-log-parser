d6972bbf319fb5d08e880ffe8928b0117c5a0d90
Tom Watson <thomasgabriel.watson@gmail.com>
2021-01-11 09:51:02 -0800

    Clean up for MVP

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 )
@@ -17,33 +18,22 @@ var maxPathsFlag int
 func init() {
 	flag.StringVar(&inFlag, "in", "log.txt", "input text file name")
 	flag.StringVar(&outFlag, "out", "results.json", "output json file name")
-	flag.IntVar(&maxClientIpsFlag, "max-client-ips", 2, "integer that defines the maximum number of results to output in the top_client_ips field")
-	flag.IntVar(&maxPathsFlag, "max-paths", 2, "integer that defines the max number of results to output on the top_path_avg_seconds field.")
+	flag.IntVar(&maxClientIpsFlag, "max-client-ips", 10, "integer that defines the maximum number of results to output in the top_client_ips field")
+	flag.IntVar(&maxPathsFlag, "max-paths", 10, "integer that defines the max number of results to output on the top_path_avg_seconds field.")
 }
 
 func main() {
 	flag.Parse()
 
+	// validate intFlags
 	if !IntFlagIsValid(maxPathsFlag) || !IntFlagIsValid(maxClientIpsFlag) {
 		fmt.Println("Flags invalid")
 		os.Exit(1)
 	}
 
-	// setup output struct
-	// read flags
+	// Consider validation of inFlag and outFlag
 
-	// read in file, line by line bufio.Scanner
-
-	// -in string
-	// -out string
-	// -max-client-ips int
-	// -max-paths int
-	// Read given file or from STDIN
-	// var logReader io.Reader
-	// var err error
-
-	// Read from STDIN and use log_format to parse log records
-
+	// open input file
 	file, errFile := os.Open(inFlag)
 
 	if errFile != nil {
@@ -52,42 +42,37 @@ func main() {
 	}
 	defer file.Close()
 
+	// scan file line by line, parse lines and append to matches
 	scanner := bufio.NewScanner(file)
-
 	var matches Matches
-
 	for scanner.Scan() {
 		match := ParseLine(scanner.Text())
 		if match == nil {
 			failed := []string{}
-			matches = append(matches, failed)
+			matches = append(matches, failed) // done for type consistency
 		} else {
 			matches = append(matches, match)
 		}
 	}
 
-	results := CompileResults(matches)
-	// sort/trim results and port to JSON
-	// write to file
-	transformedResults := TransformResults(results, maxClientIpsFlag, maxPathsFlag)
-
-	// Looking pretty good to here
-	// - Need to figure out an efficient way to port the Pair structs into JSONable format
-	// - Then I need to marshall into JSON and file to file
-	
-	json_representation, errJson := json.Marshal(transformedResults)
-
-	fmt.Println(string(json_representation))
-
-	if errJson != nil {
-
-		log.Fatal(errJson)
-}
-
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	// fmt.Println(inFlag)
-	// fmt.Println(outFlag)
+	// compile matches from file into intermediate format
+	results := CompileResults(matches)
+	// sort/trim results and port to JSON
+	transformedResults := TransformResults(results, maxClientIpsFlag, maxPathsFlag)
+	json_representation, errJson := json.MarshalIndent(transformedResults, "", " ")
+	
+	if errJson != nil {
+		log.Fatal(errJson)
+	}
+
+	fmt.Println(string(json_representation))
+	// write to output file
+	errWrite := ioutil.WriteFile(outFlag, json_representation, 0644)
+	if errWrite != nil {
+		log.Fatal(errWrite)
+	}
 }
